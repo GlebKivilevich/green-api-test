1f20feb4da3d8f6ad33068bdef0f4dbe963bcb72
Gleb Kivilevich <gleb1106f@gmail.com>
2023-05-14 21:47:51 +0400

    validation refinement / checking cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import "./App.css";
 import Auth from "./pages/Auth";
-import { Routes, Route  } from "react-router-dom";
+import { Routes, Route, useNavigate  } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  function checkAuthCookie() {
+    const authCookie = document.cookie.split(";");
+    let obj = {};
+
+    authCookie.forEach((el) => {
+      let item = el.trim().split("=");
+      obj[item[0]] = `${item[1]}`;
+    });
+
+    if (obj.idInstance && obj.apiTokenInstance) {
+      navigate("/auth");
+    } else {
+      navigate("/home");
+    }
+  } 
+
+  useEffect(() => {
+    checkAuthCookie();
+  }, []);
+
   return (
     <>
-    <Routes>
-      <Route path="/auth" element={<Auth/>}/>
-      <Route path="/" element={<Homepage/>}/>
-      {/* <Route path="*" element={}/> */}
-    </Routes>
+
     </>
     )
 }
