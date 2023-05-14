100d9f290a012d64656af5316832b4967bb33ae3
Gleb Kivilevich <gleb1106f@gmail.com>
2023-05-14 22:48:23 +0400

    made an exit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,13 +2,16 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ContactList from "../components/ContactList";
 import Chat from "../components/Chat";
+
+import { RxExit } from "react-icons/rx";
+
 const Homepage = () => {
   const navigate = useNavigate();
 
-  function checkAuthCookie() {
-    const authCookie = document.cookie.split(";");
-    let obj = {};
+  const authCookie = document.cookie.split(";");
+  let obj = {};
 
+  function checkAuthCookie() {
     authCookie.forEach((el) => {
       let item = el.trim().split("=");
       obj[item[0]] = `${item[1]}`;
@@ -23,17 +26,57 @@ const Homepage = () => {
     checkAuthCookie();
   }, []);
 
+  const exitChat  = ()  => {
+    checkAuthCookie();
+    const keysCookieObj = Object.keys(obj);
+    const valuesCookieObj = Object.values(obj);
+    console.log(obj);
+    let idInstanceExitKey;  
+    let apiTokenInstanceExitKey;
+
+    let idInstanceExitValue;
+    let apiTokenInstanceExitValue;
+
+    keysCookieObj.forEach((el) => {
+      console.log(el);
+      if (el === "idInstance") {
+        idInstanceExitKey = el;
+      }
+      if (el === "apiTokenInstance") {
+        apiTokenInstanceExitKey = el;
+      }
+    });
+
+    valuesCookieObj.forEach((el) => {
+      console.log(el);
+      if (el === `${obj.idInstance}`) {
+        idInstanceExitValue = el;
+      }
+      if (el === `${obj.apiTokenInstance}`) {
+        apiTokenInstanceExitValue = el;
+      }
+    });
+    document.cookie = `${idInstanceExitKey}=${idInstanceExitValue}; max-age= -1`;
+    document.cookie = `${apiTokenInstanceExitKey}=${apiTokenInstanceExitValue}; max-age= -1`;
+    navigate("/auth");
+  };
+
   return (
     <div className="flex justify-center m-auto">
       <div className="max-w-[380px] w-full border-r-2 border-l-2 border-gray-300">
-        <div className="bg-gray-100 py-2 px-2 flex min-h-[60px] items-center border-b-2 border-gray-400">
-          <div className="mr-5 max-w-[50px] max-h-[50px] h-full w-full rounded-full">
-            <img
-              className="max-w-[50px]  rounded-full"
-              src="https://media.istockphoto.com/id/805012064/photo/portrait-of-mature-hispanic-man.jpg?s=612x612&w=0&k=20&c=AfNa1ay8LPzaO-NOJZspqULvYavyhXg4rnJ9U_tvJY4="
-            />
+        <div className="bg-gray-100 py-2 px-2 min-h-[60px] items-center border-b-2 border-gray-400 flex justify-between">
+          <div className="flex items-center">
+            <div className="mr-5 max-w-[50px] max-h-[50px] h-full w-full rounded-full">
+              <img
+                className="max-w-[50px]  rounded-full"
+                src="https://media.istockphoto.com/id/805012064/photo/portrait-of-mature-hispanic-man.jpg?s=612x612&w=0&k=20&c=AfNa1ay8LPzaO-NOJZspqULvYavyhXg4rnJ9U_tvJY4="
+              />
+            </div>
+            <p className="text-xl">Ivan</p>
           </div>
-          <p className="text-xl">Ivan</p>
+          <button className="mr-1.5" onClick={exitChat}>
+            <RxExit fontSize={20} color="red" />
+          </button>
         </div>
         <div
           className="max-w-[380px] w-full h-full overflow-auto"
